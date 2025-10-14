9b7b5ffcf6d0b02cb2bf3e6eb20e71309bbd8581
vinsalazar09 <vinsalazar09@gmail.com>
2025-10-13 22:22:03 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,22 @@
-const CACHE = 'estudos-pwa-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  './cronograma.json'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(c => c.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+const CACHE='estudos-pwa-v3full-2';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./cronograma.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{
+  e.waitUntil((async()=>{
+    const keys=await caches.keys();
+    await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));
+    await self.clients.claim();
+  })());
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', e => {
-  const req = e.request;
+self.addEventListener('fetch',e=>{
+  const req=e.request;
   e.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).then(res => {
-      const copy = res.clone();
-      if (req.method === 'GET' && res.ok) {
-        caches.open(CACHE).then(c => c.put(req, copy));
+    caches.match(req).then(cached=>cached||fetch(req).then(res=>{
+      const copy=res.clone();
+      if(req.method==='GET' && res.ok){
+        caches.open(CACHE).then(c=>c.put(req, copy));
       }
       return res;
-    }).catch(() => caches.match('./')))
+    }).catch(()=>caches.match('./')))
   );
 });
